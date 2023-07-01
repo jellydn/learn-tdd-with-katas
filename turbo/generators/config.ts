98b567ae05f8b1844d3d7f2977ce8ec604b8ca35
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-02 00:25:27 +0800

    feat: add vitest runner for tdd practice

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -3,12 +3,12 @@ import { PlopTypes } from "@turbo/gen";
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
 	plop.setGenerator("tdd-kata", {
 		description:
-			"Create a new TDD Kata with a README.md, index.ts, and index.test.ts",
+			"Create a new TDD Kata project with a README, index file, test file, package.json, and vite.config.ts",
 		prompts: [
 			{
 				type: "input",
-				name: "file",
-				message: "What is the name of the new file to create?",
+				name: "package",
+				message: "What is the name of the new pkg to create?",
 				validate: (input: string) => {
 					if (input.includes(".")) {
 						return "file name cannot include an extension";
@@ -23,35 +23,41 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 				},
 			},
 			{
-				type: "list",
-				name: "type",
-				message: "What type of file should be created?",
-				choices: [".js", ".ts"],
-			},
-			{
 				type: "input",
 				name: "title",
-				message: "What should be the title of the new file?",
+				message: "What should be the title of the new package?",
 			},
 		],
 		actions: [
 			// Create README.md
 			{
 				type: "add",
-				path: "tdd/{{file}}/README.md",
+				path: "packages/{{package}}/README.md",
 				templateFile: "templates/README.md.hbs",
 			},
 			// Create index file
 			{
 				type: "add",
-				path: "tdd/{{file}}/index{{type}}",
-				templateFile: "templates/index{{type}}.hbs",
+				path: "packages/{{package}}/index.ts",
+				templateFile: "templates/index.ts.hbs",
 			},
 			// Create test file
 			{
 				type: "add",
-				path: "tdd/{{file}}/index.test{{type}}",
-				templateFile: "templates/index.test{{type}}.hbs",
+				path: "packages/{{package}}/index.test.ts",
+				templateFile: "templates/index.test.ts.hbs",
+			},
+			// create package.json
+			{
+				type: "add",
+				path: "packages/{{package}}/package.json",
+				templateFile: "templates/package.json.hbs",
+			},
+			// create vitest.config.ts
+			{
+				type: "add",
+				path: "packages/{{package}}/vitest.config.ts",
+				templateFile: "templates/vite.config.ts.hbs",
 			},
 		],
 	});
