32f60878446cfd9f7cf9d03afb01e316aee00bf6
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-05 23:12:49 +0800

    feat: add fizz buzz tdd template

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,9 +1,8 @@
 import { PlopTypes } from "@turbo/gen";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-	plop.setGenerator("tdd-kata", {
-		description:
-			"Create a new TDD Kata project with a README, index file, test file, package.json, and vite.config.ts",
+	plop.setGenerator("TDD Kata - String Calculator", {
+		description: "sum numbers from a string",
 		prompts: [
 			{
 				type: "input",
@@ -33,7 +32,7 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 			{
 				type: "add",
 				path: "packages/{{package}}/README.md",
-				templateFile: "templates/README.md.hbs",
+				templateFile: "templates/string-calculator-README.md.hbs",
 			},
 			// Create index file
 			{
@@ -45,7 +44,67 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 			{
 				type: "add",
 				path: "packages/{{package}}/index.test.ts",
-				templateFile: "templates/index.test.ts.hbs",
+				templateFile: "templates/string-calculator.test.ts.hbs",
+			},
+			// create package.json
+			{
+				type: "add",
+				path: "packages/{{package}}/package.json",
+				templateFile: "templates/package.json.hbs",
+			},
+			// create vitest.config.ts
+			{
+				type: "add",
+				path: "packages/{{package}}/vitest.config.ts",
+				templateFile: "templates/vite.config.ts.hbs",
+			},
+		],
+	});
+
+	plop.setGenerator("TDD Kata - Fizz Buzz", {
+		description: "print fizz buzz base on a number",
+		prompts: [
+			{
+				type: "input",
+				name: "package",
+				message: "What is the name of the new pkg to create?",
+				validate: (input: string) => {
+					if (input.includes(".")) {
+						return "file name cannot include an extension";
+					}
+					if (input.includes(" ")) {
+						return "file name cannot include spaces";
+					}
+					if (!input) {
+						return "file name is required";
+					}
+					return true;
+				},
+			},
+			{
+				type: "input",
+				name: "title",
+				message: "What should be the title of the new package?",
+			},
+		],
+		actions: [
+			// Create README.md
+			{
+				type: "add",
+				path: "packages/{{package}}/README.md",
+				templateFile: "templates/fizz-buzz-README.md.hbs",
+			},
+			// Create index file
+			{
+				type: "add",
+				path: "packages/{{package}}/index.ts",
+				templateFile: "templates/index.ts.hbs",
+			},
+			// Create test file
+			{
+				type: "add",
+				path: "packages/{{package}}/index.test.ts",
+				templateFile: "templates/fizz-buzz.test.ts.hbs",
 			},
 			// create package.json
 			{
