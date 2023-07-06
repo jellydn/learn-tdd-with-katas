a60cf3e08071c0e3c1dcc06cc5fd73712c709682
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-07 04:44:22 +0800

    feat: add roman numerals kata

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -120,4 +120,64 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 			},
 		],
 	});
+
+	plop.setGenerator("TDD Kata - Roman Numerals", {
+		description: "convert number to roman numerals",
+		prompts: [
+			{
+				type: "input",
+				name: "package",
+				message: "What is the name of the new pkg to create?",
+				validate: (input: string) => {
+					if (input.includes(".")) {
+						return "file name cannot include an extension";
+					}
+					if (input.includes(" ")) {
+						return "file name cannot include spaces";
+					}
+					if (!input) {
+						return "file name is required";
+					}
+					return true;
+				},
+			},
+			{
+				type: "input",
+				name: "title",
+				message: "What should be the title of the new package?",
+			},
+		],
+		actions: [
+			// Create README.md
+			{
+				type: "add",
+				path: "packages/{{package}}/README.md",
+				templateFile: "templates/roman-numerals-README.md.hbs",
+			},
+			// Create index file
+			{
+				type: "add",
+				path: "packages/{{package}}/index.ts",
+				templateFile: "templates/index.ts.hbs",
+			},
+			// Create test file
+			{
+				type: "add",
+				path: "packages/{{package}}/index.test.ts",
+				templateFile: "templates/roman-numerals.test.ts.hbs",
+			},
+			// create package.json
+			{
+				type: "add",
+				path: "packages/{{package}}/package.json",
+				templateFile: "templates/package.json.hbs",
+			},
+			// create vitest.config.ts
+			{
+				type: "add",
+				path: "packages/{{package}}/vitest.config.ts",
+				templateFile: "templates/vite.config.ts.hbs",
+			},
+		],
+	});
 }
